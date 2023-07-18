5cdc0298148aed5349b3894b4fc4e2ca4cddc62f
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-18 00:22:18 -0400

    firebase

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+const App = () => {
+  useEffect(() => {
+    // Firestore örneğini oluştur
+    const db = firebase.firestore();
+
+    // Firestore işlemlerini gerçekleştir
+    // Örnek: Veri ekleme
+    db.collection('myCollection').add({
+      title: 'Example Data',
+      description: 'This is an example data entry.',
+    });
+
+    // Örnek: Veri sorgulama
+    db.collection('myCollection')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, ' => ', doc.data());
+        });
+      });
+  }, []);
+
+  firestore.collection('restaurants').add(item)
+  .then((docRef) => {
+    console.log('Döküman başarıyla eklendi. Döküman ID:', docRef.id);
+  })
+  .catch((error) => {
+    console.error('Döküman eklenirken bir hata oluştu:', error);
+  });
+);
+
+  
+};
+
+export default App
